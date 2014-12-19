3a771d5ebc3d5a30776103804ec5ab784d2b9adf
rodrigoIdei4 <rodrigo@idei4.com.br>
2014-12-19 15:26:21 -0200

    Adicionado o formulario de orcamento via angularjs

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var app = angular.module('app',['ngRoute']);
 app.config(['$routeProvider',function($routeProvider){
 	$routeProvider.
 	when('/', {controller:'contatoController', templateUrl: 'contato.html'}).
-	when('/orcamento', {controller: 'orcamentoController', tempplateUrl: 'orcamento.html'}).
+	when('/orcamento', {controller:'orcamentoController', templateUrl: 'orcamento.html'}).
 	otherwise({redirectTo: '/'});
 }]);
 
